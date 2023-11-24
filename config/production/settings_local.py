5d4758752d00d5be69a17f5492f1bdf40c30406a
Ayan Nanda Goswami <ayan@spliceglobal.com>
2023-11-25 00:24:47 +0530

    update prod settings_local.py

--- config/production/settings_local.py
+++ config/production/settings_local.py
@@ -1,3 +1,15 @@
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.66.241.205']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'karan_enterprise_db',
+        'USER': 'admin',
+        'PASSWORD': 'p2#)fg%Qst)5Y8b',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
